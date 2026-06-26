8c4f20665da3aaba143b581526cddf0f1f5513d2
Vanshika <vanshikachhabra25@gmail.com>
2026-06-27 04:14:44 +0530

    Configure Firebase for Render

--- gestiva_complete/backend/firebase.py
+++ gestiva_complete/backend/firebase.py
@@ -1,8 +1,15 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceKey.json")
+    if os.getenv("FIREBASE_CREDENTIALS"):
+        firebase_creds = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+        cred = credentials.Certificate(firebase_creds)
+    else:
+        cred = credentials.Certificate("serviceKey.json")
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()

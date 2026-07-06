a207ffec87d5ede930fe9d7e271a6802acebce9e
Vanshika Chhabra <vanshikachhabra25@gmail.com>
2026-07-06 15:32:10 +0530

    Update API endpoint for risk prediction

--- gestiva_complete/src/pages/RiskPrediction.tsx
+++ gestiva_complete/src/pages/RiskPrediction.tsx
@@ -210,7 +210,7 @@ const RiskPrediction = () => {
   setResult(null);
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/api/predict-risk", {
+    const response = await fetch("https://gestiva.onrender.com/api/predict-risk", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

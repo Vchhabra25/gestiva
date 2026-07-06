ff9001f253bfb5cc82e7971e29e0ec1bb238951e
Vanshika Chhabra <vanshikachhabra25@gmail.com>
2026-07-06 15:39:57 +0530

    Update Profile.tsx

--- gestiva_complete/src/pages/Profile.tsx
+++ gestiva_complete/src/pages/Profile.tsx
@@ -26,7 +26,7 @@ const Profile = () => {
 
   const handleSave = async () => {
   try {
-    const response = await fetch("http://127.0.0.1:5000/api/profile", {
+    const response = await fetch("https://gestiva.onrender.com/api/profile", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
